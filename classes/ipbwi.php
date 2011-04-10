c5a942afb41fc2e995de2b46647f543dab86fd05
smgladkovskiy <SMGladkovskiy@gmail.com>
2011-04-10 11:57:39 +0400

    add singleton instantiating method

--- classes/ipbwi.php
+++ classes/ipbwi.php
@@ -1,3 +1,26 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
-class Ipbwi extends Ipbwi_Core {}
+class Ipbwi extends Ipbwi_Core {
+
+	/**
+	 * Instance storage
+	 * @var Ipbwi
+	 */
+	protected static $instance;
+
+	/**
+	 * Singleton instantiating
+	 *
+	 * @static
+	 * @return Ipbwi
+	 */
+	public static function instance()
+	{
+
+		if (! isset (self::$instance))
+		{
+			self::$instance = new Ipbwi();
+		}
+		return self::$instance;
+	}
+}
